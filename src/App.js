d486885054e3c8811608270e1ba8a76d50491d70
Jenson500 <mail@jens-traeger.de>
2026-06-29 13:41:14 +0200

    text changed

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Hello World! This is a test for CICD.
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
